6d9849d442b22276b171f48986a28aad671baf4e
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-09-17 21:01:39 +0300

    create TasteSwiper component for a slider

---
+++ src/components/TasteSwiper.js
@@ -0,0 +1,26 @@
+import { React } from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid } from 'swiper/modules';
+import 'swiper/css/grid';
+import 'swiper/css';
+import '../css/taste.css';
+
+import Img1 from '../img/slider1-1-morning.webp';
+import Img2 from '../img/slider1-2-origin.webp';
+import Img3 from '../img/slider1-3-everyday.webp';
+import Img4 from '../img/slider1-4-bold.webp';
+import Img5 from '../img/slider1-5-honey.webp';
+import Img6 from '../img/slider1-6-natural.webp';
+
+
+
+const TasteSwiper = () => {
+  return (
+    <div>
+      slider
+    </div>
+  )
+}
+
+export default TasteSwiper;
