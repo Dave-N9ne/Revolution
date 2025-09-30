6e50ea7760330301592e9425c3ff149609fe2229
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-09-30 23:22:13 +0300

    delete draft

--- src/components/Giftset.js
+++ src/components/Giftset.js
@@ -1,9 +1,23 @@
-import {React} from 'react'
+import {React, useState} from 'react';
+import GiftsetTabs from './Giftset-Tabs';
 
 const Giftset = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  function clickHandler(id) {
+    setActiveTab(id)
+  }
+
   return (
-    <section className="giftset">
-      giftset
+    <section id="giftset" className="giftset">
+      <div class="container giftset__container">
+        <span class="giftset__slogan slogan">Best Gift For Best Friend</span>
+        <h2 class="giftset__title title">GIFTSET</h2>
+        <GiftsetTabs 
+          activeTab={activeTab}
+          clickHandler={clickHandler}
+        />
+      </div>
     </section>
   )
 }
---
+++ src/components/GiftsetTabsData.js
@@ -0,0 +1,26 @@
+import {React} from 'react';
+
+import Img1 from '../img/giftset-gift.webp';
+import Img2 from '../img/giftset-gift-inverted.webp';
+import Img3 from '../img/giftset-gift-black-&-white.webp';
+
+export const GiftsetTabsData = [
+  {
+    img: Img1,
+    price: '285.000',
+    subTitle: 'Giftset "Cà phê phin Việt Nam"',
+    desc: 'Món quà tuyệt vời dành cho người sành cà phê. Hạt cà phê được Revo cẩn trọng chọn lựa, rang xay theo công nghệ và bí quyết học hỏi từ những nghệ nhân nổi tiếng, cùng với tình yêu, sự đam mê của người làm cà phê… tạo ra những tách cà phê tinh khiết chỉ dành riêng cho bạn.'
+  },
+  {
+    img: Img2,
+    price: '400.000',
+    subTitle: 'Giftset "Lorem ipsum dolor sit amet consectetur."',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel architecto, ducimus incidunt non voluptatem exercitationem ullam nam? Dolorem ipsa itaque unde est aut quas expedita quis autem illum, perspiciatis molestias.'
+  },
+  {
+    img: Img3,
+    price: '347.000',
+    subTitle: 'Giftset "Lorem ipsum dolor sit amet?"',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem possimus, aliquam nihil ratione non vel magni? Velit magni sed qui iusto, blanditiis dicta quis ea reiciendis adipisci eius, cupiditate expedita.'
+  }
+]
