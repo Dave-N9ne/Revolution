2f03629c16f8b8cdca5c8fc806b28de62a938850
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-10-04 18:15:15 +0300

    add combo section

--- src/components/Combo.js
+++ src/components/Combo.js
@@ -1,10 +1,20 @@
-import {React} from 'react'
+import {React} from 'react';
+import ComboSwiper from './ComboSwiper';
+
+import '../css/combo.css';
+import '../css/css-media/comboMedia.css';
 
 const Combo = () => {
   return (
-    <section className="combo">
-      combo
-    </section>
+    <section class="combo">
+        <div class="container combo__container">
+          <span lang="en" class="combo__slogan slogan">
+            Your Personalized Coffee
+          </span>
+          <h2 class="combo__title title">COMBO PHIN PHÊ</h2>
+          <ComboSwiper />
+        </div>
+      </section>
   )
 }
 
