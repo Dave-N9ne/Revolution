ede77da7355f4e7039d7b9ce13339c06eed72427
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-10-04 18:12:28 +0300

    replace data for swiper to other component

--- src/components/TasteSwiper.js
+++ src/components/TasteSwiper.js
@@ -7,60 +7,16 @@ import 'swiper/css/navigation';
 import 'swiper/css';
 import '../css/taste.css';
 
-import Img1 from '../img/slider1-1-morning.webp';
-import Img2 from '../img/slider1-2-origin.webp';
-import Img3 from '../img/slider1-3-everyday.webp';
-import Img4 from '../img/slider1-4-bold.webp';
-import Img5 from '../img/slider1-5-honey.webp';
-import Img6 from '../img/slider1-6-natural.webp';
+import { TasteSwiperData } from './TasteSwiperData.js';
 
-const slidesData = [
-  {
-    image: Img1,
-    price: '99.000',
-    subtitle: 'REVO Morning',
-    desc: 'đắng, hậu ngọt, hương hoa'
-  },
-  {
-    image: Img2,
-    price: '139.000',
-    subtitle: 'REVO Origin',
-    desc: 'hậu ngọt, ít đắng, vị chua dâu'
-  },
-  {
-    image: Img3,
-    price: '85.000',
-    subtitle: 'REVO Everyday',
-    desc: 'cân bằng, hậu đắng, vị chocolate'
-  },
-  {
-    image: Img4,
-    price: '75.000',
-    subtitle: 'REVO Đậm Đà',
-    desc: 'đậm đà, đắng, truyền thống'
-  },
-  {
-    image: Img5,
-    price: '195.000',
-    subtitle: 'REVO Honey',
-    desc: 'độ ngọt cao, hậu ngọt, chua'
-  },
-  {
-    image: Img6,
-    price: '169.000',
-    subtitle: 'REVO Natural',
-    desc: 'hậu ngọt, hương hoa, vị chua dâu'
-  },
-];
-
-const slides = slidesData.map((slide) => {
+const slides = TasteSwiperData.map((slide) => {
   return (
     <SwiperSlide>
-      <article class="taste__article flex-x">
+      <article class="taste__slide flex-x">
         <div class="taste__img">
           <img
             src={slide.image} 
-            alt=""
+            alt="coffee"
           />
         </div>
         <div class="taste__content">
@@ -73,7 +29,7 @@ const slides = slidesData.map((slide) => {
           <p class="taste__desc desc">
             {slide.desc}
           </p>
-          <ul class="taste__buttons flex-x">
+          <ul class="taste__buttons buttons flex-x">
             <li class="taste__item">
               <button class="taste__button button">Mua Ngay</button>
             </li>
@@ -96,7 +52,6 @@ const TasteSwiper = () => {
         modules={[Navigation, Grid]}
         spaceBetween={30} 
         autoHeight={false}
-        // centeredSlides={true}
         slidesPerView={window.innerWidth <= 992 ? 1 : 2} 
         navigation={{
           prevEl: '.swiper-button-prev',
